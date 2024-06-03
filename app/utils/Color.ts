583cc115c0d2270fb8923701c52db8255e8f74f9
ash <zhou@hm.edu>
2024-06-03 22:02:53 +0200

    globally defined colors

---
+++ app/utils/Color.ts
@@ -0,0 +1,25 @@
+import { on } from "events";
+
+const colors =
+{
+    primary: '#2B4B51', //Dark Green
+    secondary: '#82B1B0', //Yellow
+    background: '#FFFFFF',
+    surface: '#EDF4F3',
+    surfaceOutline: '#82B1B0',
+
+    tagRed: '#E56C6C',
+    tagOrange: '#E6AE5A',
+    tagYellow: '#E4D971',
+    tagGreen: '#79D49D',
+    tagBlue: '#83ADED',
+    tagPurple: '#BD82D9',
+    onTagRed: '#A13B3B',
+    onTagOrange: '#8A6926',
+    onTagYellow: '#7E7630',
+    onTagGreen: '#266940',
+    onTagBlue: '#32578E',
+    onTagPurple: '#70398A',
+};
+
+export default colors;
