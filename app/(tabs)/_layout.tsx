ce7dc74c11afc507c5763a1437961c8a39146f2c
nilstesch <nils.tesch03@gmail.com>
2024-06-05 14:41:18 +0200

    refactor: change single to double quotes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 const TabsLayout = () => {
     return (
@@ -9,22 +9,30 @@ const TabsLayout = () => {
             tabBarActiveTintColor: "#2B4B51"
             }}>
             <Tabs.Screen name="index" options={{
-                headerTitle: "Start",
+                headerTitle: "",
                 headerTitleAlign: "center",
                 headerTintColor: "white",
                 headerStyle: {
-                    backgroundColor: "#2B4B51"
+                    backgroundColor: "#2B4B51",
                 },
                 title: "Start",
                 tabBarIcon: ({ focused, color }) => (
                     <View style={{
                       borderRadius: 50,
-                      backgroundColor: focused ? '#2B4B51' : 'transparent',
-                      padding: 6
+                      backgroundColor: focused ? "#2B4B51" : "transparent",
+                      padding: 6,
                     }}>
-                      <Ionicons size={22} name="home-outline" color={focused ? 'white' : color} />
+                      <Ionicons size={22} name="home-outline" color={focused ? "white" : color} />
                     </View>
-                  )
+                ),
+                headerLeft: () => <Ionicons size={22} name="settings" color={"white"} style={styles.settingsIcon} />,
+                headerRight: () => (
+                    <View style={styles.headerIconsRight}>
+                        <Ionicons size={26} name="help-circle-outline" color={"white"}/>
+                        <Ionicons size={22} name="notifications" color={"white"} />
+                    </View>
+                ),
+                headerShadowVisible: false
             }} />
             <Tabs.Screen name="appointments" options={{
                 headerTitle: "Termine",
@@ -37,12 +45,13 @@ const TabsLayout = () => {
                 tabBarIcon: ({ focused, color }) => (
                     <View style={{
                         borderRadius: 50,
-                        backgroundColor: focused ? '#2B4B51' : 'transparent',
+                        backgroundColor: focused ? "#2B4B51" : "transparent",
                         padding: 6
                     }}>
-                        <Ionicons size={22} name="calendar-outline" color={focused ? 'white' : color} />
+                        <Ionicons size={22} name="calendar-outline" color={focused ? "white" : color} />
                     </View>
-                ) 
+                ),
+                headerShadowVisible: false
             }} />
             <Tabs.Screen name="chat" options={{
                 headerTitle: "Chat",
@@ -55,12 +64,13 @@ const TabsLayout = () => {
                 tabBarIcon: ({ focused, color }) => (
                     <View style={{
                         borderRadius: 50,
-                        backgroundColor: focused ? '#2B4B51' : 'transparent',
+                        backgroundColor: focused ? "#2B4B51" : "transparent",
                         padding: 6
                     }}>
-                        <Ionicons size={22} name="chatbox-outline" color={focused ? 'white' : color} />
+                        <Ionicons size={22} name="chatbox-outline" color={focused ? "white" : color} />
                     </View>
-                ) 
+                ),
+                headerShadowVisible: false
             }} />
             <Tabs.Screen name="classes" options={{
                 headerTitle: "Kurse",
@@ -73,12 +83,13 @@ const TabsLayout = () => {
                 tabBarIcon: ({ focused, color }) => (
                     <View style={{
                         borderRadius: 50,
-                        backgroundColor: focused ? '#2B4B51' : 'transparent',
+                        backgroundColor: focused ? "#2B4B51" : "transparent",
                         padding: 6
                     }}>
-                        <Ionicons size={22} name="people-outline" color={focused ? 'white' : color} />
+                        <Ionicons size={22} name="people-outline" color={focused ? "white" : color} />
                     </View>
-                ) 
+                ),
+                headerShadowVisible: false
             }} />
             <Tabs.Screen name="files" options={{
                 headerTitle: "Dateien",
@@ -91,15 +102,27 @@ const TabsLayout = () => {
                 tabBarIcon: ({ focused, color }) => (
                     <View style={{
                         borderRadius: 50,
-                        backgroundColor: focused ? '#2B4B51' : 'transparent',
+                        backgroundColor: focused ? "#2B4B51" : "transparent",
                         padding: 6
                     }}>
-                        <Ionicons size={22} name="folder-outline" color={focused ? 'white' : color} />
+                        <Ionicons size={22} name="folder-outline" color={focused ? "white" : color} />
                     </View>
-                ) 
+                ),
+                headerShadowVisible: false
             }} />
         </Tabs>
     )
 };
+
+const styles = StyleSheet.create({
+    settingsIcon: {
+        marginLeft: 20
+    },
+    headerIconsRight: {
+        flexDirection: "row",
+        gap: 15,
+        marginRight: 20
+    }
+})
 
 export default TabsLayout;
